87c223c20cacf2b2daf71437afba69826577bb90
Jinsei <jinseinai@gmail.com>
2019-05-30 18:02:44 -0700

    added file for responsiveness

--- assets/JS/media.js
+++ assets/JS/media.js
@@ -9,3 +9,12 @@ function resize(media) {
         console.log("it's fine");
     }
 }
+
+// ----------------------
+// let media = window.innerWidth;
+// resize(media);
+// media.addListener(resize);
+
+// function resize(media) {
+//     console.log("this is the screen width " + media);
+// }
