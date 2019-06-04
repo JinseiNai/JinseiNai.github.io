5e1327e79c3ad57d8a18df2883b5c8a2834561a4
Calvin <jinseinai@gmail.com>
2019-06-03 20:21:08 -0700

    responsive addition/removing of classes

--- assets/JS/media.js
+++ assets/JS/media.js
@@ -1,20 +1,21 @@
+let numOfProjects = document.querySelectorAll('.imgDiv').length;
 let media = window.matchMedia("(max-width: 768px");
 resize(media);
 media.addListener(resize);
 
 function resize(media) {
+    // let myElement = document.getElementById("imgDiv");
     if (media.matches) {
         console.log("resizing");
+        for (let i = 0; i < numOfProjects; i++) {
+            document.getElementById(`imgDiv${[i]}`).className += " s12";
+            document.getElementById(`imgDiv${[i]}`).classList.remove('s4');
+        }
     } else {
         console.log("it's fine");
+        for (let i = 0; i < numOfProjects; i++) {
+            document.getElementById(`imgDiv${[i]}`).className += " s4";
+            document.getElementById(`imgDiv${[i]}`).classList.remove('s12');
+        }
     }
 }
-
-// ----------------------
-// let media = window.innerWidth;
-// resize(media);
-// media.addListener(resize);
-
-// function resize(media) {
-//     console.log("this is the screen width " + media);
-// }
