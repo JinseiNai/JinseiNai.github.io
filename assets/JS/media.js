c115cc8021782a859256b5a7ca2b4289c14cf0ee
Calvin <jinseinai@gmail.com>
2019-06-03 22:38:53 -0700

    resizing

--- assets/JS/media.js
+++ assets/JS/media.js
@@ -1,4 +1,4 @@
-let numOfProjects = document.querySelectorAll('.imgDiv').length;
+let numOfProjects = document.querySelectorAll(".imgDiv").length;
 let media = window.matchMedia("(max-width: 768px");
 resize(media);
 media.addListener(resize);
@@ -9,13 +9,13 @@ function resize(media) {
         console.log("resizing");
         for (let i = 0; i < numOfProjects; i++) {
             document.getElementById(`imgDiv${[i]}`).className += " s12";
-            document.getElementById(`imgDiv${[i]}`).classList.remove('s4');
+            document.getElementById(`imgDiv${[i]}`).classList.remove("s4");
         }
     } else {
         console.log("it's fine");
         for (let i = 0; i < numOfProjects; i++) {
             document.getElementById(`imgDiv${[i]}`).className += " s4";
-            document.getElementById(`imgDiv${[i]}`).classList.remove('s12');
+            document.getElementById(`imgDiv${[i]}`).classList.remove("s12");
         }
     }
 }
